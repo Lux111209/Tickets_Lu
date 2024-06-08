29e2ec76b246f7bd12ea3b01ab6db46702e78687
Luz Gaspario <20200448@ricaldone.edu.sv>
2024-06-08 15:51:05 -0600

    Cambios base

--- Tickets_Lu.sql
+++ Tickets_Lu.sql
@@ -6,12 +6,11 @@ contrasenaUsuario varchar2(20) not null
 
 Create table Ticket (
 UUID_Ticket varchar2(150) not null,
-num_Ticket int not null,
 Titulo varchar2(75) not null,
 Descripcion varchar2(200) not null,
 Autor varchar2(50) not null,
 Email_autor varchar2(150) not null,
-Fecha_ticket date not null,
-Estado_ticket varchar2(150) not null,
-Fecha_fin_ticket date not null
+Fecha_ticket date not null
 );
+
+select * from Usuario
