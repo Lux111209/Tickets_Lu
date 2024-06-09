2e67bfe09467a262f499d5fedc34cf6e2d97e1fa
Luz Gaspario <20200448@ricaldone.edu.sv>
2024-06-08 20:42:52 -0600

    Finalicé

--- Tickets_Lu/app/src/main/java/luz/gaspario/tickets_lu/Login.kt
+++ Tickets_Lu/app/src/main/java/luz/gaspario/tickets_lu/Login.kt
@@ -68,6 +68,7 @@ class Login : AppCompatActivity() {
                     startActivity(pantallaLogin)
                 }
             }
+
         }
     }
 }
--- Tickets_Lu/app/src/main/java/luz/gaspario/tickets_lu/MainActivity.kt
+++ Tickets_Lu/app/src/main/java/luz/gaspario/tickets_lu/MainActivity.kt
@@ -67,6 +67,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 }
